4a41511cf00ddb0250d7be8b02ed30e42a13a0eb
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-07-10 22:58:50 -0500

    Completed Reforactoring

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,172 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+// function GameObject(attributes){
+//     this.createdAt = attributes.createdAt;
+//     this.name = attributes.name;
+//     this.dimensions = attributes.dimensions;
+//     }
+//     GameObject.prototype.destroy = function(){
+//       return `${this.name} was removed from the game.`;
+//     }
+
+class GameObject{
+    constructor(attributes){
+    this.createdAt = attributes.createdAt;
+    this.name = attributes.name;
+    this.dimensions = attributes.dimensions;
+    };
+    destroy(){
+    return `${this.name} was removed from the game.`;
+    };
+};
+
+// function CharacterStats(stats){
+//   GameObject.call(this, stats);
+//   this.healthPoints = stats.healthPoints;
+// }
+// CharacterStats.prototype = Object.create(GameObject.prototype);
+// CharacterStats.prototype.takeDamage = function(){
+//   return `${this.name} took damage.`;
+// }
+
+class CharacterStats extends GameObject {
+    constructor(stats){
+    super(stats);
+    this.healthPoints = stats.healthPoints;
+    };
+    takeDamage(){
+    return `${this.name} took damage.`;
+    }; 
+};
+// function Humanoid(human){
+//     CharacterStats.call(this, human);
+//     this.team = human.team;
+//     this.weapons = human.weapons;
+//     this.language = human.language;
+// } 
+// Humanoid.prototype = Object.create(CharacterStats.prototype);
+// Humanoid.prototype.greet = function(){
+//     return `${this.name} offers a greeting in ${this.language}.`; 
+// }
+
+class Humanoid extends CharacterStats {
+    constructor(human){
+    super(human);
+    this.team = human.team;
+    this.weapons = human.weapons;
+    this.language = human.language;
+    };
+    greet(){
+    return `${this.name} offers a greeting in ${this.language}.`; 
+    };
+};
+
+/*
+    * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+    * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+    const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+        'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+    });
+
+    const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+        'Giant Sword',
+        'Shield',
+    ],
+    language: 'Common Tongue',
+    });
+
+    const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+        'Bow',
+        'Dagger',
+    ],
+    language: 'Elvish',
+    });
+
+    // Stretch// * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+    const laser = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 4,
+    }, 
+    healthPoints: 20,
+    name: 'Buzz',
+    team: 'Space Boys',
+    weapons: [
+        'Laser Gun',
+        'Light Saber',
+    ],
+    language: 'Space Talk',
+    });
+
+    const shield = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 4,
+    }, 
+    healthPoints: 20,
+    name: 'Dark Vader',
+    team: 'Evil Click',
+    weapons: [
+        'Photon Blaster',
+        'Shield Bomb',
+    ],
+    language: 'Space Talk',
+    });
+
+
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.healthPoints); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.team); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
